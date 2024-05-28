c22ca5217a1bd63f1cea625a971247461a78e3b4
Alexio Aaron <junioralexio607@gmail.com>
2024-05-28 09:47:18 +0000

    update

--- models/staging/stg_hotel_bookings_data_partitioned.sql
+++ models/staging/stg_hotel_bookings_data_partitioned.sql
@@ -2,13 +2,14 @@ with
 
 source as (
 
-    select * from {{ source('staging', 'external_hotelbookings_data') }}
+    select * from {{ source('staging', 'hotel_bookings_data_partitioned') }}
 
 ),
 
 renamed as (
 
     select
+    
      {{ dbt_utils.generate_surrogate_key(['user_id','checkin']) }} as booking_id,
         user_id,
         checkin,
